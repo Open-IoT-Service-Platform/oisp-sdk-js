15e53c93f3a6589682a84c22f1ed4da552c9a084
Scott Ware <scott.r.ware@intel.com>
2019-03-14 15:52:29 +0000

    MQTT: Bugfixes and formatting fixes
    Removed logging from SDK as this should be handled in application code.
    Use more simple error handling in callbacks until a better solution is found.
    Fix formating issues so tests pass
    
    Signed-off-by: Scott Ware <scott.r.ware@intel.com>

--- api/mqtt/connector.js
+++ api/mqtt/connector.js
@@ -17,9 +17,10 @@
 "use strict";
 var mqtt = require('mqtt');
 var events = require('events');
-var errBuilder = require('../errorHandler/errorHandler').errBuilder;
 var fs = require('fs')
-function Broker(conf, logger) {
+var common = require('../../lib/common');
+
+function Broker(conf) {
     var me = this;
     me.host = conf.host;
     me.port = conf.port;
@@ -34,7 +35,6 @@ function Broker(conf, logger) {
     };
     me.max_retries = conf.retries || 30;
     me.messageHandler = [];
-    me.logger = logger;
     me.topics = conf.topics;
     me.pubArgs = {
         qos: conf.qos || 1,
@@ -47,20 +47,6 @@ function Broker(conf, logger) {
         connected: false,
         end: function() {}
     };
-
-    function buildPath (path, data) {
-        var re = /{\w+}/;
-        var pathReplace = path;
-        if (Array.isArray(data)) {
-            data.forEach(function (value) {
-                pathReplace = pathReplace.replace(re, value);
-            });
-        } else {
-            pathReplace = pathReplace.replace(re, data);
-        }
-        return pathReplace;
-    }
-
     me.setCredential = function (newCrd) {
         me.crd = newCrd || me.crd;
         me.credential = {
@@ -74,9 +60,8 @@ function Broker(conf, logger) {
         var me = this;
         var retries = 0;
         var hash = new Date().getTime();
-        var healthyTopic = buildPath(me.iamHealthyTopic, hash);
-        var receivingPing = function (topic, message) {
-            me.logger.info("receivingPing ", message);
+        var healthyTopic = common.buildPath(me.iamHealthyTopic, hash);
+        var receivingPing = function () {
             me.isLive = true;
         };
         var sendPing = function () {
@@ -89,17 +74,14 @@ function Broker(conf, logger) {
         var waitForPong = function () {
             if (!me.isLive) {
                 retries++;
-                me.logger.debug("Waiting for Pong # " + retries);
                 if (retries < 10) {
                     setTimeout(waitForPong, 1500);
                 } else {
-                    me.logger.error('Pong Not Achieved, Require re Attach Listeners');
                     me.unbind(healthyTopic);
                     me.emit("reconnect");
                 }
                 return false;
             }
-            me.logger.info('Pong Received');
             setTimeout(sendPing, 30000);
             return true;
         };
@@ -109,7 +91,6 @@ function Broker(conf, logger) {
             });
         }
         me.on('reconnect', function() {
-            me.logger.debug('Restarting Pinging ');
             bindingPing();
         });
         bindingPing();
@@ -120,10 +101,9 @@ function Broker(conf, logger) {
             try {
                 message = JSON.parse(message);
             } catch (e) {
-                me.logger.error('Invalid Message: %s', e);
                 return;
             }
-            me.logger.info('STATUS: %s', topic, message);
+
             me.onMessage(topic, message);
         });
         if (me.pingActivate) {
@@ -133,53 +113,36 @@ function Broker(conf, logger) {
     me.connect = function (done) {
         var retries = 0;
         try {
-           if ((me.client instanceof mqtt.MqttClient) === false) {
-               if (me.secure === false) {
-                   me.logger.info("Non Secure Connection to "+ me.host + ":" + me.port);
-                   //me.client = mqtt.createClient(me.port, me.host, me.credential);
-                   me.client = mqtt.connect('mqtt://' + me.host + ':' + me.port, {
+            if ((me.client instanceof mqtt.MqttClient) === false) {
+                if (me.secure === false) {
+                    me.client = mqtt.connect('mqtt://' + me.host + ':' + me.port, {
                         username: me.crd.username,
                         password: me.crd.password
                     });
-               } else {
-                   me.logger.debug("Trying with Secure Connection to" + me.host + ":" + me.port);
-
+                } else {
                     me.client = mqtt.connect('mqtts://' + me.host + ":" + me.port, {
                         username: me.crd.username,
                         password: me.crd.password,
                         ca : fs.readFileSync(conf.ca),
                         rejectUnauthorized: false
                     });
-
-                    me.client.on('error', function(err) {
-                        console.log(err);
-                    });
-                    //me.client = mqtt.createSecureClient(PORT, options);
-
-                    //me.client = mqtt.createSecureClient(me.port, me.host, options)
-                    //me.client = mqtt.connect(options);
                 }
             }
         } catch(e) {
-            console.log(e)
-            logger.error(e)
-            logger.error("Error in connection ex: ", e);
-            done(errBuilder.build(errBuilder.Errors.Generic.Connection.Timeout));
+            done(new Error("Connection Error", 1002));
             return;
         }
         function waitForConnection() {
             if (!me.client.connected) {
                 retries++;
-                me.logger.info("Waiting for MQTTConnector to connect # " + retries);
                 if (retries < me.max_retries) {
                     setTimeout(waitForConnection, 1500);
                 } else {
-                    me.logger.info('MQTTConnector: Error Connecting to '+  me.host + ':' + me.port);
-                    done(errBuilder.build(errBuilder.Errors.Generic.Connection.MaxTriesReached));
+                    done(new Error("Connection Error", 1001));
                 }
                 return false;
             }
-            me.logger.info('MQTTConnector: Connection successful to ' + me.host + ':' + me.port);
+
             me.listen();
             if (done) {
                 done(null);
@@ -189,7 +152,6 @@ function Broker(conf, logger) {
         waitForConnection();
     };
     me.disconnect = function () {
-        me.logger.info("Trying to disconnect ");
         me.client.end();
         me.client = {
             connected: false,
@@ -205,10 +167,9 @@ function Broker(conf, logger) {
         return a.test(text);
     }
     me.dettach = function (topic) {
-       me.logger.debug('Filtering Topics ' + topic + ' from local dispatcher');
-       me.messageHandler = me.messageHandler.filter(function (obj) {
-                                                    return !tryPattern(obj.t, topic);
-                            });
+        me.messageHandler = me.messageHandler.filter(function (obj) {
+            return !tryPattern(obj.t, topic);
+        });
     };
     me.onMessage = function (topic, message) {
         var i,
@@ -217,10 +178,9 @@ function Broker(conf, logger) {
          * Iterate over the messageHandler to match topic patter,
          * and dispatch message to only proper handler
          */
-        for (i = 0; i < length; i++ ){
+        for (i = 0; i < length; i++ ) {
             var obj = me.messageHandler[i];
             if (tryPattern(obj.t, topic)) {
-                me.logger.info('Fired STATUS: ' + topic + JSON.stringify(message));
                 obj.h(topic, message);
             }
         }
@@ -232,15 +192,12 @@ function Broker(conf, logger) {
          */
         var toCallBack = callback;
         function connectCallback() {
-            me.logger.debug('Subscribing to: ' + topic);
             me.client.subscribe(topic, {qos: 1}, function (err, granted) {
-                me.logger.debug("grant " + JSON.stringify(granted));
                 var topicAsPattern = granted[0].topic.replace(/\+/g, "[^<>]*");
-                me.logger.info("Topic Pattern :" + topicAsPattern);
                 me.attach(topicAsPattern, handler);
                 if (toCallBack) {
                     toCallBack();
-               }
+                }
             });
         }
         if (!me.connected()) {
@@ -248,7 +205,6 @@ function Broker(conf, logger) {
                 if (!err) {
                     connectCallback();
                 } else {
-                    me.logger.error(err);
                     if (toCallBack) {
                         toCallBack(err);
                     }
@@ -259,15 +215,12 @@ function Broker(conf, logger) {
         }
     };
     me.unbind = function (topic, callback) {
-        me.logger.debug('Unbinding from Topic ' + topic);
         me.client.unsubscribe(topic, function() {
-            me.logger.info('Unbound (UNSUBACK) of topic ' + topic);
+            me.dettach(topic);
+            if (callback) {
+                callback();
+            }
         });
-        me.logger.debug('Filtering Topics ' + topic + ' from local dispatcher');
-        me.dettach(topic);
-        if (callback) {
-            callback();
-        }
     };
     /**
      * @description publish broadcast to an specifics topics, the message.
@@ -283,7 +236,6 @@ function Broker(conf, logger) {
             options = options || me.pubArgs;
         }
         function publishCallback() {
-            me.logger.debug('Publishing : T => ' + topic + " MSG => " + JSON.stringify(message));
             me.client.publish(topic, JSON.stringify(message), options, callback);
         }
         if (!me.connected()) {
@@ -291,10 +243,9 @@ function Broker(conf, logger) {
                 if (!err) {
                     publishCallback();
                 } else {
-                    me.logger.error("The connectio has faild on publish " + err);
                     if (callback) {
                         callback(err);
-                     }
+                    }
                 }
             });
         } else {
@@ -309,9 +260,9 @@ function Broker(conf, logger) {
 Broker.prototype = new events.EventEmitter();
 
 var broker = null;
-module.exports.singleton = function (conf, logger) {
+module.exports.singleton = function (conf) {
     if (!broker) {
-        broker = new Broker(conf, logger);
+        broker = new Broker(conf);
     }
     return broker;
 };
